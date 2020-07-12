f250dc216559953deae56d51846e706cd5926916
UmedJadhav <umed.jadhav@somaiya.edu>
2020-07-12 18:15:40 +0530

    Added sign in form component

--- src/pages/login-logout/login-logout-component.jsx
+++ src/pages/login-logout/login-logout-component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './login-logout-styles.scss';
-
+import SignIn from '../../components/sign-in/sign-in-component';
 const LoginLogoutPage = () => (
-  <div className='sign-in-and-sign-up'>SIGN IN</div>
+  <div className='sign-in-and-sign-up'>
+    <SignIn/>
+  </div>
 );
 
 export default LoginLogoutPage;
