49ee145a769655f9ca86a9fd82c2e5b6c775fc27
UmedJadhav <umed.jadhav@somaiya.edu>
2020-07-26 12:41:59 +0530

    Added sign up component to the project .

--- src/pages/login-logout/login-logout-component.jsx
+++ src/pages/login-logout/login-logout-component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './login-logout-styles.scss';
 import SignIn from '../../components/sign-in/sign-in-component';
+import SignUp from '../../components/sign-up/sign-up-component';
+
 const LoginLogoutPage = () => (
-  <div className='sign-in-and-sign-up'>
+  <div className='login-logout'>
     <SignIn/>
+    <SignUp/> 
   </div>
 );
 
