f76988ce62f800b1e15ca09f418f2f17a9883323
UmedJadhav <umed.jadhav@somaiya.edu>
2020-08-17 20:53:50 +0530

    Added redux-persist to store cart state in local Storage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter} from 'react-router-dom';
-
- import { Provider } from 'react-redux';
-import store from './redux/store';
+import { BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { persistGate, PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
