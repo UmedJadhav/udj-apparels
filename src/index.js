1f7b190d5d24e646832645c27532f65d8f862c42
UmedJadhav <umed.jadhav@somaiya.edu>
2020-08-17 22:16:00 +0530

    Moved directory section data to its own reducer Refactored directory component to func component

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
-import { persistGate, PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
